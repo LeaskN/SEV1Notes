23ba5e14d06da4f6595c3061ff41a79bc95bc4fe
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-07 13:45:56 -0400

    started objects notes

---
+++ dataTypes/objects/objects.js
@@ -0,0 +1,31 @@
+//OBJECTS
+    // Objects are comprised of Key-Value pairs.
+        // key on the left of the colon 
+        // value on the right of the colon
+    // Declaring an object
+        // Example:
+            // obj = {
+            //      name: 'Nic',
+            //      interests: [ 'Ali', 'Soccer', 'Coding', 'Noah' ],
+            //      age: 68,
+            //}
+    // Accessing information 
+        // Example: Get the name
+            // obj.name === 'Nic'
+
+    var personObj = {
+        name: 'Nic',
+        interests: [ 'Ali', 'Soccer', 'Coding', 'Noah' ],
+        age: 68,
+    };
+
+    // Accessing information in an object
+    // console.log(personObj.name);
+    // console.log(personObj.interests);
+    // console.log(personObj.age);
+
+    console.log(personObj[3]);
+
+
+
+
