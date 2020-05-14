0b9a079c33855381561d549f45253fe993b586d9
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-14 16:28:18 -0400

    added a homework folder

---
+++ dataTypes/Homework/5.15.20.js
@@ -0,0 +1,27 @@
+// 5.15.20
+
+// Example:
+
+        // ONLY using comments write a function that turns an array into a string
+
+        // Example Solution:
+
+            // Declare a function that takes in an array
+
+            // The function will join the array into one long string using spaces
+
+// Q1:
+    // Write a function that takes in a string and turns it into an array based on where it has dashes
+        // Input: var string = "Pizza-Pizza-Give-Me-All-The-Pizza!"
+        // Output: ["Pizza","Pizza","Give","Me","All","The","Pizza!"]
+
+//Q2:
+    // Write a function that takes in an array of strings and joins it all together without spaces
+        // Input: var arr = ["I","Can","Not","Wait","For", "Warm", "Weather!"]
+        // Output: "ICanNotWaitForWarmWeather!"
+
+// Q3:
+    // Write a function that takes in a string and removes all of its dashes
+        // Input: var str = "I-Love-To-Code!"
+        // Output: "ILoveToCode!"
+        // Hint: this is the previous two questions combined
