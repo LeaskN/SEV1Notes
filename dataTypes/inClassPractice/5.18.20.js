f1fef62158308a5ccfa515e2d4b5a7f846b17ff8
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-18 15:47:04 -0400

    eod 5-18

--- dataTypes/inClassPractice/5.18.20.js
+++ dataTypes/inClassPractice/5.18.20.js
@@ -6,6 +6,13 @@
     //         Good Morning World!
     //         Goodnight World!
 
+    // var str = 'Hello World!';
+
+    // function greeting(param){
+    //     return param;
+    // }
+    // console.log(greeting(str));
+
 // Level 1:
 
     // Write out 3 functions which take in a number and perform a mathematical operation and return the result
@@ -13,12 +20,105 @@
         // Multiply
         // Divide
 
+        // function addNumbers(x, y){
+        //     return (x+y);
+        // }
+        // console.log(addNumbers(6, 4));
+
     // Write out 3 functions which take in a string and return 
         // The first character 
         // The last character
         // The middle character
 
+        var whyLord = 'This is crazy';
+        // var str1 = 'Test, test, this is a test.';
+        // var str2 = 'Run Forrest, run!';
+        
+        // function firstChar(param){
+        //     var firstLetter = param.slice(0,1);
+        //     return firstLetter;
+        // }
+
+        // console.log(firstChar(whyLord));
+
+        // function middleChar(param){
+        //     var middleLetter = param.slice(param.length / 2, (param.length / 2) + 1);
+        //     return middleLetter;
+        // }
+
+        // var str = "HEYA-YOU!";
+
+        // function getLastCharacter(strParam){
+        //     var start = strParam.length - 1;
+        //     var end = strParam.length;
+        //     var item1 = strParam.slice(start, end);
+        //     return item1;
+        // }
+
+        // var str = "HEYA-YOU!";
+        // var str1 = 'Test, test, this is a test.';
+        // var str2 = 'Run Forrest, run!';
+
+        // function getMiddleChar(strParam){
+        //     var start = strParam.length / 2;
+        //     var end = (strParam.length / 2) + 1;
+        //     var item1 = strParam.slice(start, end);
+        //     return item1;
+        // }
+
+        // console.log(getMiddleChar(str));
+        // console.log(getMiddleChar(str1));
+        // console.log(getMiddleChar(str2));
+
+        // console.log(middleChar(whyLord));
+        // console.log(middleChar(str1));
+        // console.log(middleChar(str2));
+
+        // function lastChar(param){
+        //     var lastLetter = param.slice( param.length - 1, param.length );
+        //     return lastLetter;
+        // }
+        // console.log(lastChar(whyLord));
+        // console.log(lastChar(str1));
+        // console.log(lastChar(str2));
+
     // Create 3 functions which take in an array and return 
         // The first element 
         // The last element 
         // The middle element
+    
+    var allElements1 = ['fire', 'rock', 'water'];
+    var allElements2 = ['air', 'water', 'fire', 'rock'];
+
+    // function elementOne(arrParam){
+    //     return arrParam[0];
+    // }
+    // console.log(elementOne(allElements1));
+    // console.log(elementOne(allElements2));
+
+    // function elementLast(arrParam){
+    //     var lenghOfArr = arrParam.length - 1;
+    //     var lastElement = arrParam[lenghOfArr];
+    //     return lastElement;
+    // }
+    
+    // console.log(elementLast(allElements1));
+    // console.log(elementLast(allElements2));
+
+// level 2:
+    // Write ONE function that can Add, Multiply, or Divide based on the given arguments
+    // function mathOperation(operation, a, b){
+        //     if(operation === '+'){
+        //         return a + b;
+        //     } else if (operation === '*'){
+        //         return a * b;
+        //     } else if (operation === '/'){
+        //         return a / b;
+        //     } else {
+        //         return 'Operation Not Available'
+        //     }
+        // }
+        // console.log(mathOperation('+', 10, 3));
+        // console.log(mathOperation('*', 10, 3));
+        // console.log(mathOperation('/', 10, 3));
+        // console.log(mathOperation('^', 10, 3));
