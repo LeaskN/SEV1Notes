698db8d4a04505167f4c741b18e07c331e971e70
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-19 09:07:56 -0400

    5.19.20 morning

---
+++ dataTypes/Homework/5.18.20.js
@@ -0,0 +1,10 @@
+// Homework:
+    // Read:
+    //     https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+    //     SKIP: A “Hello World!” example
+    //     SKIP: Events section
+    //     SKIP/STOP HERE: Supercharging our example website
+    //     SKIP: Conclusion
+
+// Write down 7 questions. 
+    // Write them down as soon as they come to you.
