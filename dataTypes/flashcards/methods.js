12c8bf2027b94210e860c9ec38ecf3e2db1f9023
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-20 12:52:53 -0400

    added method definitions

---
+++ dataTypes/flashcards/methods.js
@@ -0,0 +1,62 @@
+// push
+        // Adds a new element to the end of an array
+        // Data type it works on: array
+        // Syntax arr.push(newElement)
+
+// indexOf
+        // We can find the index of a character or element
+        // Data type it works on: Strings and Arrays
+        // Syntax: arr.indexOf(element) or str.indexOf(character)
+
+  // slice
+        // Returns the selected elements in an array or in a string
+        // Data type it works on: Strings and Arrays
+        // Syntax: array.slice(start, end)
+        // Syntax: string.slice(start, end)
+
+ // length
+        // All strings have a length
+        // All arrays also have a length
+        // Data type it works on: Strings and Arrays
+        // Syntax: array.length
+        // Syntax: string.length
+
+// replace
+        // Replaces the first instance of a substring within a string
+        // Data type it works on: Strings
+        // Syntax: string.replace('oldSubStr', 'newSubStr')
+
+// unshift
+        // Adds new items to the beginning of an array
+        // Data type it works on: Arrays
+        // Syntax: array.unshift(newElement)
+
+// shift
+        // Removes the first item of an array
+        // Data type it works on: Arrays
+        // Syntax: array.shift()
+
+    // pop
+        // Removes the last item of an array.
+        // Data type it works on: Arrays
+        // Syntax: array.pop()
+
+    // join
+        // Used to join the elements of the array together into a string.
+        // Data type it works on: Arrays
+        // Syntax: array.join('-')
+
+    // splice
+        // Adds and Removes items to and from an array and returns the removed items.
+        // Data type it works on: Array
+        // Syntax: array.splice(index, numberOfEleToDelete, newElements)
+
+    // toUpperCase
+        // Converts a string to uppercase letters.
+        // Data type it works on: Strings
+        // Syntax: string.toUpperCase()
+
+    // toLowerCase()
+        // Converts a string to lowercase letters.
+        // Data type it works on: Strings 
+        // Syntax: string.toLowerCase()
