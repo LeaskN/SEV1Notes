1c43e75b7691f6cc5daaac4f47f8f1377b62c5cd
Nicholas Leask <leask.nicholas@gmail.com>
2020-05-27 10:40:27 -0400

    added conditional in class practice

--- homework/5.26.20.js
+++ homework/5.26.20.js
@@ -18,12 +18,62 @@
     // Q 2.1
         // Create a function which returns 'big' if the given number is greater than or equal to 10.
         // If it is smaller, return 'small'. 
-    // Q 2.1
-        // Create a function which returns 'No such value' if an object doesn't have a value.
-        // If it does, return that value. 
+
+    // Q 2.2
+        // Create a function which takes in an object 
+        // If the object doesn't have a specific key return 'No such value'.
+        // If it does, return that key's value. 
+
+        let dirtBike1 = {
+            brand: 'yamaha', 
+            stroke: 2, 
+            breaks: ['foot', 'hand'], 
+            gas: 'unleaded'
+        }
+            
+        let dirtBike2 = {
+            brand: 'honda', 
+            stroke: 2, 
+            breaks: ['foot', 'hand'], 
+            gas: 'unleaded',
+            cylinder: '1.0'
+        }
+            
+        let dirtBike3 = {
+            brand: 'kawasaki', 
+            stroke: 2, 
+            breaks: ['foot', 'hand'], 
+            gas: 'unleaded',
+        }
+            
+        function checkCylinder(param){
+            if(param.cylinder === undefined){
+                return 'no such value'; 
+            } else {
+                return param.cylinder; 
+            }
+        }
+            
+        // console.log(checkCylinder(dirtBike1)); 
+        // console.log(checkCylinder(dirtBike2)); 
+        // console.log(checkCylinder(dirtBike3));
 
 // level 3
     // Q 3.1
         // Create a function which takes in two numbers and compares them. 
         // If the first number is bigger return the first number.
         // If the second number is bigger return the second number.
+
+    function name(param1, param2){
+        if(param1 > param2){
+            return param1;
+        } else if (param1 < param2){
+            return param2;
+        } else {
+            return 'The numbers are equal';
+        }
+    }
+
+    console.log(name(3, 6));
+    console.log(name(2, 2));
+    console.log(name(1, -5));
---
+++ inClassPractice/5.27.20.js
@@ -0,0 +1,38 @@
+// Conditionals
+
+// level 0
+    // Q0.1
+        // Create a function which takes in a string. 
+        // If the string length is greater than 0 return 'The string exists'. 
+        // Else return, 'The string doesn't have characters'.
+
+// level 1
+    // Q 1.1  
+        // Create a function which takes in two numbers 
+        // If number one is larger than number two return 'Number one is greater than number two.'
+        // If number two is larger than number one return 'Number two is greater than number one.'
+
+// level 2
+    // Q 2.1
+        // Create a function which takes the following array: [99, 'Long Island', 'Hello World']; 
+        // If the array contains the string 'Hello World' the function returns 'Hello World!'.
+        // If 'Hello World doesn't exist return 'Goodnight World'.  
+            // Once you think you have the solution also try and invoke the function with a different array: ['red', 'green', 'periwinkle']; 
+    
+    // Q 2.2
+        // Create a function which takes in a car object.
+        // If the object contains a key of carName with a value of 'Ferrari'
+        // and the object contains a key of color with a value of 'Red'.
+        // return 'Whoa, a classic Ferrari!'.
+        // else return 'Meh, it's just a car'.
+
+// level 3
+    // Q 3.1
+        // Create a function which takes in a car object.
+        // If the object contains a key of carName with a value of 'Lamborghini'
+        // and the object contains a key of color with a value of 'yellow'.
+        // return 'Whoa, a classic Lamborghini!'.
+        // If the carName is 'Lamborghini' and the color isn't 'yellow' then return, 'That's an interesting Lamborghini.'
+        // else return 'Meh, it's just a car'.
+
+    
