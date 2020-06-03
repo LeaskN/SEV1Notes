0f4b7cbdc899abc2c64b63f2fb5825d84c6ee341
Nicholas Leask <leask.nicholas@gmail.com>
2020-06-03 13:57:59 -0400

    added half of todays questions

---
+++ JavaScript_Content/inClassPractice/6.3.20.js
@@ -0,0 +1,33 @@
+// loops or conditionals (focus on picking ONE or the other)
+
+// level 0
+// loop warmup
+    // create a function which iterates over an array of strings 
+    // the function returns a new array of captalized strings
+
+// conditional warmup
+    // create a function which takes in a number 
+    // the function returns 'even' if the number is even and 'odd' if its odd
+
+// level 1
+    // Q1:
+        // create a function which takes in a string 
+        // the function returns even if the string has an even amount of elements
+        // otherwise it returns odd
+
+    // Q2: 
+        // create a function which takes in an array
+        // the function returns a new array with every other element in the array
+
+    // Q3:
+        // create a function which takes in an array 
+        // the function returns the first element with the character 'i'
+
+// level 2:
+    // Q1:
+        // create a function which takes in two numbers
+        // it returns 'That was divisible!' if the first number is divisble by the second
+
+    // Q2: 
+        // create a function which takes in two arrays
+        // the 
