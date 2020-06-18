c11d20fed8040a2fdeef1834491b388276c815ad
Nicholas Leask <leask.nicholas@gmail.com>
2020-06-18 11:27:20 -0400

    added more obj loop questions

--- JavaScript_Content/notes/dataTypes/objects/objectsPractice.js
+++ JavaScript_Content/notes/dataTypes/objects/objectsPractice.js
@@ -27,6 +27,7 @@
 //______________________________________________________________________________________________________________
 
 // Object Loop practice
+// Practice 3
 // level 0:
     // create an object (minimum 3 key-value pairs) & create a function which iterates over the object and logs all values to the console.
 
@@ -39,3 +40,20 @@
     // create an object & create a function which logs all boolean key-value pairs to the console 
 
     // create an object & create a function which logs all array key-value pairs to the console 
+
+// level 3: 
+    // given the following object create a function which logs the username & password length.
+        let account = {
+            username: "Snowman",
+            password: "caRR0t"
+        }
+
+    // given the following object create a function which logs the house address & passcode as stars.
+        // input: ring. Output: "Address: 1600 Pennsylvania Ave NW, Washington, DC 20500. Passcode: ****.""
+        let ring = {
+            homeAddress: "1600 Pennsylvania Ave NW, Washington, DC 20500",
+            passcode: "2020"
+        }
+
+// level 4: 
+    // create an object & create a function which increases the factor of any number by 10
