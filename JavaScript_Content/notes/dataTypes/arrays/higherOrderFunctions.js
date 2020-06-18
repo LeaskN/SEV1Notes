e5d2b4d8c670bb9792e91e17ffc193c11d377912
Nicholas Leask <leask.nicholas@gmail.com>
2020-06-18 16:01:12 -0400

    added answers to the questions

---
+++ JavaScript_Content/notes/dataTypes/arrays/higherOrderFunctions.js
@@ -0,0 +1,12 @@
+//Higher order functions:
+    // Definition:
+    // A higher order function is a function that takes a function as an argument, or returns a function. 
+
+// Filter 
+
+// Reduce
+
+// Map
+
+// Sort
+
