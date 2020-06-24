e9fcb062fe54aa181d08439cc0ad41d7dbbc9405
Nicholas Leask <leask.nicholas@gmail.com>
2020-06-24 15:47:19 -0400

    added 6.24.20 hw

---
+++ JavaScript_Content/homework/6.24.20.js
@@ -0,0 +1,94 @@
+// Use bracket or dot notation to answer the questions below.  
+// Some questions will involve writing functions
+
+let stringArr = ["first", "second", "middle", "last"]; 
+
+//1) console.log the first element of the 'stringArr' array
+
+//2) console.log the last element of the 'stringArr' array
+
+//3) add a new element to the end of the 'stringArr' array called "new last"
+
+//4) console.log the last element of the 'stringArr' array. 
+// note: your solution should work for both questions 2 & 4. 
+
+//5) console.log the middle element of the 'stringArr' array
+// note: extra points if you use a dynamic solution (hint: Math.floor) 
+// your console should read "middle" 
+
+//6) add a new element to the beginning of the 'stringArr' array called "new first"
+
+
+//7) console.log the 2nd index of the 'stringArr' array 
+
+//8) console.log the 0 index of the 'stringArr' array
+
+//9) console.log the length of the 'stringArr' array
+
+//10) console.log the length of every string within the 'stringArr' array.  
+//note: use only one console.log to achieve this... you will need to write a function. 
+
+let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+let newNumArr = []
+
+//11) within a console.log, add the first and last index values of 'numArr' array
+//ex. console.log(firstIndex + lastIndex); 
+
+//12) remove the last index value of the 'numArr' array 
+
+//13) add the number 0 to the beginning of the 'numArr' array
+//result: numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//14) remove the last index value from the 'numArr' array
+//result: numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+//15) console.log the middle index dynamically.  
+//hint(Math.floor) 
+
+let arrOfArr = [['this', 'is', 'an', 'array'], 
+                [1, 2, 3, 4, 5], 
+                ['another', 'array', 'of', 'strings'],
+                [6, 7, 8, 9, 0] ]
+
+
+//16) console.log the numbers 1 & 6 from the 'arrOfArr' array. 
+//note: do NOT console.log(1, 6) <- this is not the answer. 
+
+//17) console.log both 'array' strings from the 'arrOfArr' array
+
+//18) console.log the two arrays that contain only numbers
+//note: your console should contain 2 arrays 
+
+//19) console.log the two arrays taht contain only strings
+//note: your console should contain 2 arrays 
+
+//20) console.log the word "strings" from the 'arrOfArr' array
+
+//21) console.log the number 0 from the 'arrOfArr' array
+
+//22) console.log the word "is" from the 'arrOfArr' array
+
+//23) console.log the number 3 from the 'arrOfArr' array
+
+//24) console.log only the strings that begin with consonants from the 'arrOfArr' array
+
+//25) console.log the number 8 from the 'arrOfArr' array
+
+//26) console.log the word 'of' from the 'arrOfArr' array
+
+//27) console.log the 1st occurence of the word 'array' from the 'arrOfArr' array
+
+//28) console.log the 2nd occurence of the word 'array' from the 'arrOfArr' array
+
+//29) write a function that returns the first elements of EACH array from the 'arrOfArr' array
+//result: (['this', 1, 'another', 6])
+
+
+//30) write a function that returns the last elements of EACH array from the 'arrOfArr' array 
+//result: (['array', 5, 'strings', 0])
+
+
+
+
+
