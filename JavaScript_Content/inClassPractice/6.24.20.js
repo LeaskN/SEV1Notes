15870b96c1ad925716f4a57fff0bde80c32e805c
Nicholas Leask <leask.nicholas@gmail.com>
2020-06-24 11:07:01 -0400

    added in class practice for .map()

---
+++ JavaScript_Content/inClassPractice/6.24.20.js
@@ -0,0 +1,21 @@
+// higher order function practice
+// Map
+
+// level 0: using .map() do the following
+    // double the values of any array of numbers
+
+// level 1: using .map() do the following
+    // grab the first letter of an array of strings
+    // grab the first element of an array of arrays
+
+// level 2: using .map() do the following
+    // grab the last letter of an array of strings
+    // grab the last letter of an array of arrays
+
+// level 3: using .map() do the following
+    // create a new array of fullNames from an array of objects where ONE object looks similar to this:{first: 'Nic', last: 'Leask'}
+    // Input: [{first: 'Ro', last: 'Pili'}, {first: 'Nic', last: 'Leask'}]
+    // Output: ['Ro Pili', 'Nic Leask']
+
+// level 4: using .map() do the following
+    // 
